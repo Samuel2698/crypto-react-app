34a55b9d4274cd867153770f8460ec427a0d2f89
Samuel Velasco <samuelvelasco2698@gmail.com>
2022-06-28 12:37:29 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from  'react'
+import React, { useEffect, useState } from  'react'
 import {Routes, Route} from 'react-router-dom'
 import Navbar from './components/Navbar'
 import { ThemeProvider } from './context/ThemeContext';
@@ -9,13 +9,22 @@ import Account from './routes/Account'
 import axios from 'axios'
 
 function App() {
+  const [coins, setCoins] = useState([])
 
+  const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=10&page=1&sparkline=true'
+
+  useEffect(() => {
+    axios.get(url).then((response) => {
+      setCoins(response.data)
+      console.log(response.data)
+    })
+  },[url])
 
   return (
     <ThemeProvider>
       <Navbar />
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='/' element={<Home coins={coins} />} />
         <Route path='/signin' element={<Signin />} />
         <Route path='/signup' element={<Signup />} />
         <Route path='/account' element={<Account />} />
--- src/components/CoinSearch.jsx
+++ src/components/CoinSearch.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
+import CoinItem from './CoinItem'
 
-const CoinSearch = () => {
+const CoinSearch = ({coins}) => {
+  const [searchText, setSearchText] = useState('')
+  // console.log(coins)
   return (
-    <div>
-        
+    <div className='rounded-div my-4'>
+        <div className='flex flex-col md:flex-row justify-between pt-4 pb-6 text-center md:text-right'>
+          <h1 className='text-2xl font-bold my-2'>Trouver une cryptomonnaie</h1>
+          <form action="">
+            <input
+              className='w-full bg-primary border border-input px-4 py-2 rounded-2xl shadow-xl'
+              onChange = {(e) => setSearchText(e.target.value)} 
+              type="text" 
+              placeholder='Rechercher'
+            />
+          </form>
+        </div>
+
+        <table className='w-full border-collapse text-center'>
+          <thead>
+            <tr className='border-b'>
+              <th></th>
+              <th>#</th>
+              <th>Monnaie</th>
+              <th></th>
+              <th>Cours</th>
+              <th>24 h</th>
+              <th>Volume sur 24 h</th>
+              <th>Capitalisation du marché</th>
+              <th>7 derniers jours</th>
+            </tr>
+          </thead>
+          
+          <tbody>
+            {coins
+              .filter((value) => {
+              if (searchText === '') {
+                return value
+
+              } else if (
+                value.name.toLowerCase().includes(searchText.toLowerCase())
+              ) {
+                return value
+              }
+            })
+            .map((coin) => (
+              <CoinItem key={coin.id} coin={coin} />
+            ))}
+          </tbody>
+        </table>
     </div>
   )
 }
