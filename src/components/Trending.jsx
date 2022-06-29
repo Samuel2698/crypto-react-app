70ecd231c9532b91d8224867566319f116efbfa7
Samuel Velasco <samuelvelasco2698@gmail.com>
2022-06-29 16:59:55 +0200

    Add files via upload

--- src/components/Trending.jsx
+++ src/components/Trending.jsx
@@ -14,8 +14,35 @@ const Trending = () => {
     }, [])
 
   return (
-    <div>
-      <h1>Cryptos qui font la tendance</h1>
+    <div className='rounded-div my-13 py-5 text-primary'>
+      <h1 className='text-2xl font-bold py-4'>Cryptos qui font la tendance</h1>
+      <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-4'>
+        {trending.map((coin, idx) => (
+          <div key={idx} className='rounded-div flex justify-between p-4 hover:scale-105 ease-in-out duration-300'>
+            <div className='flex w-full items-center justify-between'>
+              <div className='flex'>
+                <img 
+                  className='mr-4 rounded-full'
+                  src={coin.item.small} 
+                  alt="logo d'une cryptomonnaie" 
+                />
+              </div>
+              <div>
+                <p className='font-bold'>{coin.item.name}</p>
+                <p>{coin.item.symbol}</p>
+              </div>
+              <div className='flex items-center'>
+                <img
+                  className='w-4 mr-2' 
+                  src="https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579" 
+                  alt="logo de Bitcoin" 
+                />
+                <p>{coin.item.price_btc.toFixed(7)}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
--- src/routes/CoinPage.jsx
+++ src/routes/CoinPage.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 
 const CoinPage = () => {
+  const [coin, setCoin] = useState({})
+
+  const url = 'https://api.coingecko.com/api/v3/coins/bitcoin?sparkline=true'
+
+  useEffect(() => {
+    axios.get(url).then((response) => {
+      setCoin(response.data)
+      console.log(response.data)
+    })
+  },[url])
+
   return (
     <div>
-        
+        <div>
+          <img src={coin.image?.large} alt="/" />
+        </div>
+        <p>Prix de {coin?.name}</p>
     </div>
   )
 }
 
 export default CoinPage
+
