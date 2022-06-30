747b55fb8fd0888391e1e5c19c1f9ec5b7991a17
Samuel Velasco <samuelvelasco2698@gmail.com>
2022-06-30 14:53:53 +0200

    Add files via upload

--- src/routes/CoinPage.jsx
+++ src/routes/CoinPage.jsx
@@ -3,11 +3,14 @@ import axios from 'axios'
 import { Sparklines, SparklinesLine } from 'react-sparklines';
 import { FaTwitter, FaFacebook, FaReddit, FaGithub } from 'react-icons/fa';
 import DOMPurify from 'dompurify'
+import {useParams} from 'react-router-dom'
 
 const CoinPage = () => {
   const [coin, setCoin] = useState({})
+  const params = useParams()
 
-  const url = 'https://api.coingecko.com/api/v3/coins/bitcoin?sparkline=true'
+  const url =
+     `https://api.coingecko.com/api/v3/coins/${params.coinId}?sparkline=true` 
 
   useEffect(() => {
     axios.get(url).then((response) => {
@@ -36,7 +39,7 @@ const CoinPage = () => {
               {coin.market_data?.current_price 
                 ? (<p className='text-3xl font-bold'>{coin.market_data.current_price.eur.toLocaleString()}€</p>) 
                 : null}
-              <p>7d</p>
+              <p>7 Jours</p>
             </div>
             <div>
               <Sparklines data={coin.market_data?.sparkline_7d.price}>
@@ -45,87 +48,86 @@ const CoinPage = () => {
             </div>
             <div className='flex justify-between py-4'>
               <div>
-                <p className='text-gray-500'>Capitalisation Boursière</p>
+                <p className='text-gray-500 text-sm'>Capitalisation Boursière</p>
                 {coin.market_data?.market_cap ? (<p>{coin.market_data.market_cap.eur.toLocaleString()}€</p>) : null}
               </div>
               <div>
-                <p>Volume (24h)</p>
+                <p className='text-gray-500 text-sm'>Volume (24h)</p>
                 {coin.market_data?.market_cap ? (<p>{coin.market_data.total_volume.eur.toLocaleString()}€</p>) : null}
               </div>
             </div>
             
-              <div>
+              <div className='flex justify-between py-4'>
                 <div>
-                  <p>Prix le plus élevé (24h)</p>
+                  <p className='text-gray-500 text-sm'>Prix le plus élevé (24h)</p>
                   {coin.market_data?.high_24h ? (<p>{coin.market_data.high_24h.eur.toLocaleString()}€</p>) : null}
                 </div>
                 <div>
-                  <p>Prix le plus bas (24h)</p>
+                  <p className='text-gray-500 text-sm'>Prix le plus bas (24h)</p>
                   {coin.market_data?.low_24h ? (<p>{coin.market_data.low_24h.eur.toLocaleString()}€</p>) : null}
                 </div>
               </div>
           </div>
 
               <div>
-                <p>Statistiques de {coin.name}</p>
-
-                <div>
+                <p className='text-xl font-bold'>Statistiques de {coin.name}</p>
+                <div className='flex justify-between py-4'>
                   <div>
-                    <p>Position sur le marché</p>
+                    <p className='text-gray-500 text-sm'>Position sur le marché</p>
                     {coin.market_cap_rank}
                   </div>
                   <div>
-                    <p>Algorithme de hachage</p>
+                    <p className='text-gray-500 text-sm'>Algorithme de hachage</p>
                     {coin.hashing_algorithm ? <p>{coin.hashing_algorithm}</p> : null}
                   </div>
                   <div>
-                    <p>Score de confiance</p>
+                    <p className='text-gray-500 text-sm'>Score de confiance</p>
                     {coin.tickers ? <p>{coin.liquidity_score.toFixed(2)}</p> : null}
                   </div>
                 </div>
 
-                  <div>
+                  <div className='flex justify-between py-4'>
                     <div>
-                      <p>Changement de prix (24h)</p>
+                      <p className='text-gray-500 text-sm'>Changement de prix (24h)</p>
                       {coin.market_data ? (<p>{coin.market_data.price_change_percentage_24h.toFixed(2)}%</p>) : null}
                     </div>
                     <div>
-                      <p>Changement de prix (7j)</p>
+                      <p className='text-gray-500 text-sm'>Changement de prix (7j)</p>
                       {coin.market_data ? (<p>{coin.market_data.price_change_percentage_7d.toFixed(2)}%</p>) : null}
                     </div>
                     <div>
-                      <p>Changement de prix (14j)</p>
+                      <p className='text-gray-500 text-sm'>Changement de prix (14j)</p>
                       {coin.market_data ? (<p>{coin.market_data.price_change_percentage_14d.toFixed(2)}%</p>) : null}
                     </div>
                   </div>
 
-                  <div>
+                  <div className='flex justify-between py-4'>
                     <div>
-                      <p>Changement de prix (30j)</p>
+                      <p className='text-gray-500 text-sm'>Changement de prix (30j)</p>
                       {coin.market_data ? (<p>{coin.market_data.price_change_percentage_30d.toFixed(2)}%</p>) : null}
                     </div>
                     <div>
-                      <p>Changement de prix (60j)</p>
+                      <p className='text-gray-500 text-sm'>Changement de prix (60j)</p>
                       {coin.market_data ? (<p>{coin.market_data.price_change_percentage_60d.toFixed(2)}%</p>) : null}
                     </div>
                     <div>
-                      <p>Changement de prix (1an)</p>
+                      <p className='text-gray-500 text-sm'>Changement de prix (1an)</p>
                       {coin.market_data ? (<p>{coin.market_data.price_change_percentage_1y.toFixed(2)}%</p>) : null}
                     </div>
                   </div>
 
-                  <div>
-                      <FaTwitter />
-                      <FaFacebook />
-                      <FaReddit />
-                      <FaGithub />
+                  <div className='flex justify-around text-accent cursor-pointer'>
+                      <FaTwitter className='hover:text-blue-600'/>
+                      <FaFacebook className='hover:text-blue-600'/>
+                      <FaReddit className='hover:text-blue-600'/>
+                      <FaGithub className='hover:text-blue-600'/>
                   </div> 
             </div>       
         </div>
 
         {/* Description */}
-        <div>
-          <p>A propos de {coin?.name} (seulement en anglais)</p>
+        <div className='p-4'>
+          <p className='text-xl font-bold'>A propos de {coin?.name} (seulement en anglais)</p>
           <p dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(coin.description ? coin.description.en : ''),}}></p>
         </div>
     </div>
--- src/routes/Signin.jsx
+++ src/routes/Signin.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import {AiOutlineMail} from 'react-icons/ai'
 
 const Signin = () => {
   return (
     <div>
-        
+        <div>
+          <h1>Connexion</h1>
+          <form>
+            <div>
+              <label>E-mail</label>
+              <div>
+                <input type="email" />
+                <AiOutlineMail />
+              </div>
+            </div>
+            <div>
+              <label>Mot de passe</label>
+              <div>
+                <input type="text" />
+              </div>
+            </div>
+          </form>
+        </div>
     </div>
   )
 }
